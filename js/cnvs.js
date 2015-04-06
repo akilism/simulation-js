889271db7f7e55f8bbd5bccc38857d329358cee9
Akil Harris <akil.harris@gmail.com>
2015-04-05 21:01:32 -0400

    refactored cnvs

--- js/cnvs.js
+++ js/cnvs.js
@@ -7,8 +7,10 @@ var cnvs = (function() {
     canvasWidth,
     time = 0,
     counter = 0,
-    circles = [],
+    movers = [],
     attractors = [],
+    repellers = [],
+    particleSystems = [],
     clickX,
     clickY,
     moveX,
@@ -44,50 +46,54 @@ var cnvs = (function() {
       // canvas.addEventListener('mousemove', onMouseMove);
       xScaler = interpolater(0, 1, 0, 600);
       yScaler = interpolater(0, 1, 0, 600);
-      rScaler = interpolater(100, 400, Math.min(90, canvasWidth/8), Math.max(15, canvasWidth/24));
+      // rScaler = interpolater(100, 400, Math.min(90, canvasWidth/8), Math.max(15, canvasWidth/24));
       alphaScaler = interpolater(0, 1, 0, 255);
-      // addCircles(10);
-
+      particleSystems.push(new ParticleSystem(new Vector(canvasWidth/1.8, 150), canvasWidth, canvasHeight));
+      addRepeller();
     } else {
       isCanvasEnabled = false;
     }
   };
-
-  // var addCircles = function(numCircles) {
-  //   for(var i = 0; i < numCircles; i++) {
-  //     circles.push(new Mover(Circle, {r: 5 , color: '#dfdfdf'}, 10, new Vector(Math.random()*450, canvasHeight/2), canvasWidth, canvasHeight));
-  //   }
-  // };
 
   var getColor = function() {
       var r = Math.floor(Math.random() * 256),
       g = Math.floor(Math.random() * 256),
       b = Math.floor(Math.random() * 256),
       a = 1;
-      // r = 0; g = 0; b = 0;
-      a = 0.5;//Math.random();
+      r = 0; g = 0; b = 0;
+      a = 1;//Math.random();
       return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
   };
 
-  var addCircle = function() {
+  var addAttractor = function() {
+    var mass = 12;
+    attractors.push(new Attractor(Circle,
+      {r: mass, color: '#00FF00'},
+      mass,
+      1,
+      new Vector(canvasWidth/2, canvasHeight/2)
+    ));
+  };
+
+  var addRepeller = function() {
+    var mass = 20;
+    repellers.push(new Repeller(Circle,
+      {r: mass, color: 'rgba(140, 140, 0, .5)'},
+      mass,
+      1,
+      new Vector(canvasWidth/2, canvasHeight/2)
+    ));
+  };
+
+  var addMover = function() {
     var mass = Math.max(100, Math.round(Math.random() * 400));
-    circles.push(new Mover(Circle,
+    movers.push(new Mover(Circle,
       {r: rScaler(mass) , color: 'rgba(0,0,0,1)'},
       mass,
       new Vector(Math.random()*canvasWidth,
       canvasHeight + 300),
       canvasWidth,
       canvasHeight));
-  };
-
-  var addAttractor = function() {
-    var mass = 60;
-    attractors.push(new Attractor(Circle,
-      {r: mass, color: '#00FF00'},
-      mass,
-      1.5,
-      new Vector(canvasWidth/2, canvasHeight/2)
-    ));
   };
 
   var tick = function(delta) {
@@ -97,10 +103,10 @@ var cnvs = (function() {
 
   var render = function() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    circles.forEach(function(circle) {
-      circle.draw(ctx);
-    });
-
+    particleSystems.forEach(function(particleSystem) { particleSystem.draw(ctx); });
+    repellers.forEach(function(repeller) { repeller.draw(ctx); });
+    movers.forEach(function(mover) { mover.draw(ctx); });
+    counter++;
   };
 
   var run = function(timestamp) {
@@ -112,27 +118,17 @@ var cnvs = (function() {
     render();
   };
 
-  var flipCircleColors = function(toRandom) {
-    circles.forEach(function(circle) {
-      var color = (toRandom) ? getColor() : 'rgba(0,0,0,1)';
-      circle.setNewColor(color);
-    });
-  };
-
-
   var onClick = function(evt) {
     clickX = evt.clientX;
     clickY = evt.clientY;
     // console.log('click', clickX, clickY);
     mouseClicked = !mouseClicked;
 
-
     if(!mouseClicked) {
       ga('send', 'event', 'canvas', 'click', 'froze canvas', mouseClicked);
     } else {
       ga('send', 'event', 'canvas', 'click', 'unfroze canvas', mouseClicked);
     }
-
   };
 
   var onMouseMove = function(evt) {
@@ -149,129 +145,61 @@ var cnvs = (function() {
   };
 
   var update = function(delta) {
+    // if(counter % 10 === 0) {
+    //   particleSystems[0].addParticles(25);
+    // }
+    // particleSystems[0].applyRepellers(repellers);
+    // particleSystems[0].update();
 
     if(attractors.length < 1) {
       addAttractor();
     }
 
-    if(circles.length < 500 && counter > 20) {
+    if(movers.length < 500 && counter > 20) {
       for(var i = 0; i < 50; i++) {
-        addCircle();
+        addMover();
       }
       counter = 0;
     } else { counter++; }
-    circles.forEach(updateCircle);
+    movers.forEach(updateMover);
   };
 
-  var updateCircle = function(circle) {
+  var updateMover = function(mover) {
     /* drag and friction forces.
-    if(circle.position.y > canvasHeight - 300) {
-      var drag = circle.getDrag(0.005, 1);
-      circle.applyForce(drag);
+    if(mover.position.y > canvasHeight - 300) {
+      var drag = mover.getDrag(0.005, 1);
+      mover.applyForce(drag);
     }
 
-    var friction = circle.getFriction(0.01, 1);
-    circle.applyForce(friction);
+    var friction = mover.getFriction(0.01, 1);
+    mover.applyForce(friction);
     //*/
     if(!mouseClicked) {
       attractors.forEach(function(attractor) {
-        circle.applyForce(attractor.comeToMe(circle));
+        mover.applyForce(attractor.comeToMe(mover));
       });
 
-      circle.applyForce(new Vector(0.01, 0)); //wind
-      circle.applyForce(new Vector(0, 0.1 * circle.mass)); //gravity
+      mover.applyForce(new Vector(0.01, 0)); //wind
+      mover.applyForce(new Vector(0, 0.1 * mover.mass)); //gravity
     } else {
-      var friction = circle.getFriction(1, 10);
-      circle.applyForce(friction);
+      var friction = mover.getFriction(1, 10);
+      mover.applyForce(friction);
     }
-    circle.update();
+    mover.update();
   };
 
-  var updateCircleFreeze = function(circle) {
+  var updateMoverFreeze = function(mover) {
     if(!mouseClicked) {
       if(Math.random() > 0.5) {
-        circle.applyForce(new Vector(0.5, 0.0));
+        mover.applyForce(new Vector(0.5, 0.0));
       } else {
-        circle.applyForce(new Vector(-0.5, 0.0));
+        mover.applyForce(new Vector(-0.5, 0.0));
       }
-      circle.applyForce(new Vector(0.0, 0.0025));
+      mover.applyForce(new Vector(0.0, 0.0025));
     } else {
-      circle.applyForce(new Vector(-circle.velocity.x, -circle.velocity.y));
+      mover.applyForce(new Vector(-mover.velocity.x, -mover.velocity.y));
     }
   };
-
-  var Circle = function(opts) {
-    this.r = opts.r || 5;
-    this.color = opts.color || '#dfdfdf';
-    this.endAngle = 2*Math.PI;
-    this.position = null;
-    this.newColor = null;
-  };
-
-
-  Circle.prototype.incrementColor = function(a, b, x) {
-    if(b != a) {
-      return a += 1;
-    }
-    return a;
-    // return a * (1 - x) + b * x;
-  };
-
-  Circle.prototype.setNewColor = function(color) {
-    this.newColor = color;
-  };
-
-  Circle.prototype.getColorString = function(color) {
-    return 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + color.a + ')';
-  };
-
-  Circle.prototype.getColorObj = function(color) {
-    var colors = color.replace('rgba(', '').replace(')', '').split(',');
-
-    // if(colors[0].indexOf('r') > -1) { debugger; }
-    return {
-      r: parseFloat(colors[0]),
-      g: parseFloat(colors[1]),
-      b: parseFloat(colors[2]),
-      a: parseFloat(colors[3]),
-    };
-  };
-
-  Circle.prototype.setColor = function() {
-    if(this.newColor === this.color || !this.newColor) { return this.color; }
-
-    var newColorObj = this.getColorObj(this.newColor);
-    var oldColorObj = this.getColorObj(this.color);
-
-    var currColorObj = {
-      r: this.incrementColor(oldColorObj.r, newColorObj.r, 1),
-      g: this.incrementColor(oldColorObj.g, newColorObj.g, 1),
-      b: this.incrementColor(oldColorObj.b, newColorObj.b, 1),
-      a: 0.5
-    };
-    var colorStr = this.getColorString(currColorObj);
-    // console.log(colorStr);
-    this.color = colorStr;
-  };
-
-  Circle.prototype.draw = function(ctx) {
-    this.setColor();
-    ctx.beginPath();
-    ctx.arc(this.position.x, this.position.y, this.r, 0, this.endAngle, false);
-
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  };
-
-  Circle.prototype.hit = function(x, y) {
-    return (x <= this.position.x + this.r && x >= this.position.x - this.r) &&
-           (y <= this.position.y + this.r && y >= this.position.y - this.r);
-  };
-
-  Circle.prototype.setPosition = function(position) {
-    this.position = position;
-  };
-
 
   return {
     setCanvas:setCanvas,
